57dd7c7a9694305f4fdcf3378a99884f82b5feeb
Jason Lee <inbox.jason@gmail.com>
2014-02-06 17:26:31 +0900

    Rewrote the download code.

--- trailer_main.py
+++ trailer_main.py
@@ -47,14 +47,14 @@ import time
 configFile = 'config.conf'
 config     = ConfigObj(configFile)
 
-#Variables    
+# Program options   
 save_path_var      = config['Preferences']['path_to_save']
 quality_var        = config['Preferences']['quality']
-num_to_dl_var      = int (config['Preferences']['number_to_dl'])
-file_age_var       = config['Preferences']['old_file_age']
+num_to_dl_var      = config.get('Preferences').as_int('number_to_dl')
+file_age_var       = config.get('Preferences').as_float('old_file_age')
 custom_label_var   = config['Preferences']['custom_string']
 
-#These below are all boolean values    
+# These below are all boolean values    
 del_files_bool       = config.get('Preferences').as_bool('del_old_files')
 rename_trailer_bool  = config.get('Preferences').as_bool('rename_trailer')
 download_clips_bool  = config.get('Preferences').as_bool('download_clips')
@@ -62,18 +62,21 @@ multi_part_bool      = config.get('Preferences').as_bool('multi_part')
 verbose_output_bool  = config.get('Preferences').as_bool('verbose_output')
 log_file_bool        = config.get('Preferences').as_bool('log_file')
 
-#These are catergories from hd-trailers and boolean values   
+# These are catergories from hd-trailers and boolean values   
 download_latest_bool    = config.get('Download From').as_bool('latest')
 most_watched_bool       = config.get('Download From').as_bool('most_watched')
 top_movies_bool         = config.get('Download From').as_bool('top_movies')
 opening_this_week_bool  = config.get('Download From').as_bool('opening_this_week')
 coming_soon_bool        = config.get('Download From').as_bool('coming_soon')
+last_down = ''
+num_to_dl_var -= 1
 
-#Program prefs - load these fom file in later version!!!!!
+# Program variables
 site_pref          = 'http://movietrailers.apple.com'   #download from here
 filetype_var       = 'mov', 'mp4', 'avi'
 base_url           = 'http://www.hd-trailers.net'     
 download_count_var = 0   
+
 
 def writeDebug(debug_info):
 #******** This writes a line to the debug file as well as to the console
@@ -94,84 +97,77 @@ def  checkDirectory(directory):
         pass
     else :
         os.mkdir(save_path_var)
-        if verbose_output_bool : writeDebug('Directory created : ' + save_path_var)
+        writeDebug('Directory created : ' + save_path_var)
 
 def deleteFiles():
-	seconds_in_day = 86400
-	days_old = file_age_var * seconds_in_day	
-	del_time = time.time() - float(days_old)
-	os.chdir(save_path_var)
-	for filename in os.listdir(save_path_var):
-		cur_time = os.path.getmtime(filename)
-		if cur_time <= del_time : print filename, ' is older'  # delete here
-		if cur_time >= del_time : print filename, ' is not older'
-		#print('Simple!!!!!!!!!!!111', filename, 'is ', t, 'old',)
-        #print(t , '    ', sixty_days)
-        #print 'j'
-		#if mtime < sixty_days:
-		#    print('remove %s'%somefile)
-		#    counter += 1
-		#    print(counter, 'files to be deleted')
-
-  
+    del_count = 0
+    seconds_in_day = 86400
+    days_old = file_age_var * seconds_in_day    
+    del_time = time.time() - days_old
+    os.chdir(save_path_var)
+    for filename in os.listdir(save_path_var):
+        cur_time = os.path.getmtime(filename)
+        if cur_time <= del_time : 
+            os.remove(filename)
+            del_count += 1                       
+            WriteDebug('Deleted     : ' + filename)
+    writeDebug(str(del_count) + ' files deleted.')
+ 
 def checkLink(active_link):
 # ********* Check to see if the link contains a movie
 #           we want to download  
-#              
-    file_name = active_link.split('/')[-1] 
-    if multi_part_bool :
-        grab_type = '-tlr1'
-        if site_pref in active_link and quality_var in active_link and grab_type in active_link: # Check site and resolution
-               writeDebug("Found multipart :" + active_link)  
-               if checkDuplicate(file_name) != 'DUPE' :     # Check if the file is a duplicate 	              
-                   downloadLink(active_link)     # pass the link and download it
-        grab_type = '-tlr2'
-        if site_pref in active_link and quality_var in active_link and grab_type in active_link:
-               writeDebug("Found mulipart : " + active_link)  
-               if checkDuplicate(file_name) != 'DUPE' :    
-                    downloadLink(active_link)    
-        grab_type = '-tlr3'
+#   
+    global last_down    
+    file_name = active_link.split('/')[-1]
+    if multi_part_bool :        # Download multiple clips for the same movie
+        current_movie = active_link.split('/')[-1]      # This gets the filename from the url
+        if last_down == current_movie : 
+            return 
+        grab_type = '-tlr'
         if site_pref in active_link and quality_var in active_link and grab_type in active_link: 
-               writeDebug("Found multipart :" + active_link)  
-               if checkDuplicate(file_name) != 'DUPE' :    
-                   downloadLink(active_link)    
-        grab_type = '-tlr4'
-        if site_pref in active_link and quality_var in active_link and grab_type in active_link:
-               writeDebug("Found multipart :" + active_link)  
-               if checkDuplicate(file_name) != 'DUPE' :
-                   downloadLink(active_link)    
+            writeDebug("Found Trailer : " + active_link)  
+            if checkDuplicate(file_name) != 'DUPE' :     # Check if the file is a duplicate                  
+                downloadLink(active_link)                # pass the link and download it                  
+                last_down = active_link.split('/')[-1]   # This gets the filename
            
-    if not multi_part_bool :          
-        grab_type = '-tlr1'
-        if site_pref in active_link and quality_var in active_link and grab_type in active_link:
-               writeDebug("Found : " + active_link)                                           
-               if checkDuplicate(file_name) != 'DUPE' :
-                   downloadLink(active_link)   
-
-    if download_clips_bool :
-        grab_type = 'clip'
-        if site_pref in active_link and quality_var in active_link and grab_type in active_link: 
-             writeDebug("Found clip : " + active_link)                                                                                    
-             if checkDuplicate(file_name) != 'DUPE' :
-                 downloadLink(active_link)    
-
+                
+    if not multi_part_bool :         # Download 1 trailer for each movie
+        current_movie = active_link.split('/')[-2]      # This gets the movie name from the url
+        grab_type = '-tlr'       
+        if last_down == current_movie : 
+            return 
+        if site_pref in active_link and quality_var in active_link and  grab_type in active_link :
+            writeDebug("Found Trailer : " + active_link)                                           
+            if checkDuplicate(file_name) != 'DUPE' :
+                downloadLink(active_link)   
+                last_down = active_link.split('/')[-2]   
+            
+ 
+    if download_clips_bool :          # Download clips
+        grab_type = 'clip', '-fte1' 
+        for grab in grab_type :
+            if site_pref in active_link and quality_var in active_link and grab in active_link: 
+                writeDebug("Found clip ++ : " + active_link)                                                                                    
+                if checkDuplicate(file_name) != 'DUPE' :
+                    downloadLink(active_link)    
+               
 
 def downloadLink(url):
 # ******** Download the file to save_path_var location
 #
 # 
-    global download_count_var    
-        
-    file_name = url.split('/')[-1] 					  # Strip out the filename
+    global download_count_var     
+    file_name = url.split('/')[-1]                    # Strip out the filename
     request = urllib2.Request(url)                    # Prepare the url to be opened
     request.add_header('User-agent', 'Quicktime')     # We are now a qt player as apple only allows qt players to download their files 
     u = urllib2.urlopen(request)                      # Open the url we have created
     f = open(save_path_var + file_name, 'wb')         # f is the file we will write to (wb - write, binary)                    
-    meta = u.info()         						  # Get the metainfo from the url we have open
+    meta = u.info()                                   # Get the metainfo from the url we have open
     file_size = int(meta.getheaders("Content-Length")[0])      # Grab the filesize
-    if verbose_output_bool : print ("Downloading: %s to: %s" % (file_name, save_path_var))
+    
     file_size_dl = 0          # How much have we downloaded
     block_sz = 8192           # Block size - we will download this much at once
+    if verbose_output_bool : writeDebug("Downloading   : %s to: %s" % (file_name, save_path_var))
     while True:
         buffer = u.read(block_sz)   # Read 8192 bytes from our url
         if not buffer:
@@ -180,12 +176,12 @@ def downloadLink(url):
         f.write(buffer)                  # Write the buffer to our file
         status = r"TOTAL: %s Bytes- DONE: %4d Bytes [%3.2f%%]" % (file_size, file_size_dl, file_size_dl * 100. / file_size)   # Magic from stackexchange
         status = status + chr(8)*(len(status)+1)                                        # More magic
-        print (status),                                                                 # Print the magic   ',' keeps it all on one line
-    if verbose_output_bool : writeDebug("Downloaded - " + file_name)
+        if verbose_output_bool : print status,                                                # Print the magic   ',' keeps it all on one line
     f.close() 
     download_count_var += 1
-
-
+    print '\n'
+    writeDebug ('Downloaded    : ' + file_name)
+    
 
 def checkDuplicate(filename):
 # ********* Checks for duplicate filename
@@ -196,8 +192,8 @@ def checkDuplicate(filename):
     elif custom_label_var == '' : search_str = extracted_filename[0] + extracted_filename[1]                   # If there is no custom string
     
     for filename in os.listdir(save_path_var):    
-        if filename == search_str or filename == extracted_filename[0] + extracted_filename[1] :		# Check for duplicates. The second check if for
-            if verbose_output_bool : writeDebug('Duplicate detected, not downloading ' + filename)      # completeness. 
+        if filename == search_str or filename == extracted_filename[0] + extracted_filename[1] :        # Check for duplicates. The second check if for
+            writeDebug('Duplicate --- : ' + filename)                           # completeness. 
             return('DUPE')
             break
             
@@ -206,17 +202,18 @@ def extractFilename(filename):
 # ********** Takes a filename and returns a tuple with
 #            filename[0] and extension[1] --> 
 #
-     sizeofstr = len(filename) 		
-     sizeofstr = sizeofstr - 4		
-     extracted_file_ext = filename[sizeofstr:len(filename)]		# Extract the file extension
-     extracted_file_name = filename.replace(extracted_file_ext, '')		# Extract the filename
+     sizeofstr = len(filename)      
+     sizeofstr = sizeofstr - 4      
+     extracted_file_ext = filename[sizeofstr:len(filename)]     # Extract the file extension
+     extracted_file_name = filename.replace(extracted_file_ext, '')     # Extract the filename
      return extracted_file_name, extracted_file_ext
 
      
 def renameTrailers():
 # ********** Rename all files in the save_path_var folder,
 #            adding custom_label_var to the end of the filename    
-# 
+#  
+    rename_count = 0
     for filename in os.listdir(save_path_var):      
         if not custom_label_var in filename and not 'clip' in filename:  # Check that custom_string and clip are not in filename
              if filename.endswith(filetype_var):                         # File ext to search for
@@ -224,17 +221,16 @@ def renameTrailers():
                  extracted_filename = extractFilename(filename)
                  if extracted_filename[1] == "oad" : file_ext = ".mov"   # Apple sometimes returns files with no extension, make these .movs              
                  target = os.path.join(save_path_var, extracted_filename[0] + custom_label_var + extracted_filename[1])     # dir + file + ext
-                 os.rename(path, target) 
-                 if verbose_output_bool : writeDebug ('Renamed ' + path + ' to ' + target)
-    if verbose_output_bool : writeDebug('No files found to rename')      
-
-        
+                 os.rename(path, target)   
+                 rename_count += 1             
+    writeDebug (str(rename_count) + ' trailers renamed.' )                 
+                                             
             
 def makeNewSoup(new_link):
 # ********* Search through the pages of movies to find 
 #           links to download from apple movies
 #
-    if verbose_output_bool : writeDebug('Following url ... ' + new_link)
+    writeDebug('Following url : ' + new_link)
     new_url = requests.get(base_url + new_link)   # make a new url to scrape new page                
     data = new_url.text
     soup = BeautifulSoup(data)
@@ -244,7 +240,7 @@ def makeNewSoup(new_link):
                     if link == None : current_link = "NULL" 
                     new_current_link = link.get('href')
                     #writeDebug('Found link ... ' + new_current_link)
-                    checkLink(new_current_link)     # pass our link to be checked
+                    checkLink(new_current_link)
            except KeyError:
                    pass
 
@@ -254,32 +250,39 @@ def makeSoup(start_url):
 # ********** Make the soup - this gets back a list of links
 #                            we will search through to find trailers 
 #
-    writeDebug('*** Scraping ...  ' + start_url + ' ***')
-    url = requests.get(start_url)	# Get the page to scrape
-    data = url.text		
-    soup = BeautifulSoup(data)	# Make the soup
+    if verbose_output_bool : writeDebug('**Scraping**  : ' + start_url)
+    url = requests.get(start_url)   # Get the page to scrape
+    data = url.text     
+    soup = BeautifulSoup(data)  # Make the soup
     for link in soup.findAll('a'):                                        # find the <a href> tag
         if 'movie' in link['href'] and "autoplay" not in link['href']:    # is 'movie' in the link but not 'autoplay'
             if link == None : current_link = "NULL"                       # get rid of empty links
-            current_link = link.get('href')                               # grab the link target
-            makeNewSoup(current_link)                                     # start again with a new link to see if there is anything to grab
+            current_link = link.get('href')                               # grab the link target            
+            if download_count_var <= num_to_dl_var :
+                makeNewSoup(current_link)                                     # start again with a new link to see if there is anything to grab
+            else : 
+                writeDebug('Download limit reached.')
+                writeDebug(str(download_count_var) + ' trailers downloaded.')
+                return           
 
 def main():
-    #while download_count_var < num_to_dl_var : 
-        #if download_latest == '1' : makeSoup(base_url + '/page/1/')
-        #if most_watched : ADD IN DUPLICATE DETECTION
-     #   if top_movies_bool : makeSoup(base_url + '/top-movies/')
-        #if opening_this_week == '1' : makeSoup(base_url + '/opening-this-week/')
-        #if coming_soon == '1' : makeSoup(base_url + '/coming-soon/')
-    #else :
-      #  if verbose_output_bool : 
-       #     writeDebug('Download limit reached! ' + str(download_count_var) + ' trailers downloaded.')
-        #return 
+    if save_path_var == '' :
+        writeDebug('** No save path specified. Exiting... **')
+        writeDebug('** Please edit config.conf and        **')
+        writeDebug('** include a path to save to.         **')
+        return
+    checkDirectory(save_path_var)
     
-    #if rename_trailer_bool : renameTrailers()
-    deleteFiles()
+    if download_latest_bool   : makeSoup(base_url + '/page/1/')
+    if most_watched_bool      : makeSoup(base_url + '/most-watched')
+    if top_movies_bool        : makeSoup(base_url + '/top-movies/')
+    if opening_this_week_bool : makeSoup(base_url + '/opening-this-week/')
+    if coming_soon_bool       : makeSoup(base_url + '/coming-soon/')
     
+    if rename_trailer_bool    : renameTrailers() 
+    if del_files_bool         : deleteFiles()
     
+    if download_count_var == 0 : writeDebug('No new trailers found.')
     
 if __name__ == '__main__':
     main()
